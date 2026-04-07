2b96fe80f27bcacf58a07671a141846c10cc3421
Rushikesh <sawantr2305@gmail.com>
2026-04-07 12:08:20 +0530

    Initial commit

---
+++ scripts/aws-copy.sh
@@ -0,0 +1,21 @@
+aws s3 cp CAvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-ca/
+aws s3 cp DEvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-de/
+aws s3 cp FRvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-fr/
+aws s3 cp GBvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-gb/
+aws s3 cp INvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-in/
+aws s3 cp JPvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-jp/
+aws s3 cp KRvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-kr/
+aws s3 cp MXvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-mx/
+aws s3 cp RUvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-ru/
+aws s3 cp USvideos.csv s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics/region-us/
+
+aws s3 cp CA_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-ca/
+aws s3 cp DE_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-de/
+aws s3 cp FR_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-fr/
+aws s3 cp GB_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-gb/
+aws s3 cp IN_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-in/
+aws s3 cp JP_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-jp/
+aws s3 cp KR_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-kr/
+aws s3 cp MX_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-mx/
+aws s3 cp RU_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-ru/
+aws s3 cp US_category_id.json s3://yt-datapipeline-bronze-ap-south-1/youtube/raw-statistics-reference-data/region-us/
---
+++ scripts/info.md
@@ -0,0 +1,23 @@
+Bronze Bucket - yt-datapipeline-bronze-ap-south-1
+Silver Bucket - yt-datapipeline-silver-ap-south-1
+Gold Bucket - yt-datapipeline-gold-ap-south-1
+
+Scripts Bucket - yt-datapipeline-script-ap-south-1
+
+SNS ARN - arn:aws:sns:ap-south-1:338764933231:yt-dp-alerts:f7d0ff21-3c37-4618-a4ec-35a04c7df94e
+
+YT API KEY - AIzaSyC8M7LnRQYT4ckwFcpIacHtmlVXvKE9B-8
+
+Glue Bronze DB - yt-dp-glue-bronze
+Glue Silver DB - yt-dp-glue-silver
+Glue Gold DB - yt-dp-glue-gold
+
+--bronze_database yt-dp-glue-bronze 
+--bronze_table raw_statistics 
+--silver_bucket yt-datapipeline-silver-ap-south-1 
+--silver_database yt-dp-glue-silver 
+--silver_table clean_reference_data
+
+--silver_database yt-dp-glue-silver 
+--gold_bucket yt-datapipeline-gold-ap-south-1 
+--gold_database yt-dp-glue-gold
